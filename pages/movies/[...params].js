a3f9818962d5603a64c0568ae03eb18a9e232757
mons1220 <mons1220@gmail.com>
2022-03-02 22:37:21 +0900

    eslint disabled revise

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable */
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
